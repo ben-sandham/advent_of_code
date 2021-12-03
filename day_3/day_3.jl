94147af465d5e6450f115540499675ec1b1dd439
bsandham <bsandham@umass.edu>
2021-12-03 10:33:41 -0500

    Day 1 and 2

---
+++ day_3/day_3.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 19b1f1a4-933c-46da-85ad-7f85b3552e74
+
+
+# ╔═╡ 8a8d9ebe-53f6-11ec-02d6-499581fd238d
+
+
+# ╔═╡ c71349fa-6b4d-4dd4-bdef-b5eda14392b9
+
+
+# ╔═╡ Cell order:
+# ╠═19b1f1a4-933c-46da-85ad-7f85b3552e74
+# ╠═8a8d9ebe-53f6-11ec-02d6-499581fd238d
+# ╠═c71349fa-6b4d-4dd4-bdef-b5eda14392b9
